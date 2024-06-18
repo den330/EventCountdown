85ffe77b52e361f99971151da48374c889f52a5f
den330 <wo131485508@qq.com>
2024-06-18 18:04:49 -0400

    Initial Commit

---
+++ EventCountdown/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  EventCountdown
+//
+//  Created by yaxin on 2024-06-18.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ EventCountdown/EventCountdownApp.swift
@@ -0,0 +1,17 @@
+//
+//  EventCountdownApp.swift
+//  EventCountdown
+//
+//  Created by yaxin on 2024-06-18.
+//
+
+import SwiftUI
+
+@main
+struct EventCountdownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
