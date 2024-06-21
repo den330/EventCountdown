bef89b01f316cabd1f2c6008582e0fed08dd1df2
den330 <wo131485508@qq.com>
2024-06-21 10:19:39 -0400

    icon

--- EventCountdown/ContentView.swift
+++ EventCountdown/ContentView.swift
@@ -8,12 +8,28 @@
 import SwiftUI
 import SwiftData
 
+struct MyTextFieldStyle: TextFieldStyle {
+    func _body(configuration: TextField<Self._Label>) -> some View {
+        configuration
+        .padding(10)
+        .background(
+            RoundedRectangle(cornerRadius: 15, style: .continuous)
+                .stroke(Color.black, lineWidth: 3)
+        ).padding()
+    }
+}
+
 struct ContentView: View {
     @StateObject var viewModel: EventViewModel
-    @State private var selectedDate: Date = Date()
+    @State private var selectedDate: Date = Date().addingTimeInterval(86400)
     @State private var eventName: String = ""
+    @State private var showAlert = false
     var timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
     private func addEvent() {
+        if eventName == "" {
+            showAlert = true
+            return
+        }
         viewModel.addEvent(name: eventName, date: selectedDate)
     }
     
@@ -25,12 +41,33 @@ struct ContentView: View {
         VStack {
             VStack(spacing: 20) {
                 Text("Type in event name here:")
-                TextField("Enter event name", text: $eventName).border(Color.black).padding(.leading, 30).padding(.trailing, 30)
-                DatePicker("Select a date", selection: $selectedDate, displayedComponents: [.date, .hourAndMinute])
+                    .font(.system(size: 24, weight: .bold, design: .default))
+                    .fontDesign(.rounded)
+                    .padding()
+                    .foregroundStyle(.white)
+                    .background(Color.purple)
+                    .clipShape(RoundedRectangle(cornerRadius: 15))
+
+                TextField("Enter event name", text: $eventName)
+                    .border(.white)
+                    .textFieldStyle(MyTextFieldStyle())
+                    .padding(.horizontal, 30)
+                    .onChange(of: eventName) { _, newValue in
+                        showAlert = newValue == ""
+                    }
+                if (showAlert) {
+                    Text("Event name is required").fontDesign(.monospaced).foregroundStyle(.red)
+                }
+                DatePicker("Select a date", selection: $selectedDate, in: Date().addingTimeInterval(86400)..., displayedComponents: [.date, .hourAndMinute])
                 Button("Add Event") {
                     addEvent()
-                }.background(Color.red).font(.callout)
-            }
+                }.fontDesign(.rounded)
+                    .font(.system(size: 24, weight: .bold, design: .default))
+                    .padding(10)
+                    .background(Color.blue)
+                    .cornerRadius(15)
+                    .foregroundStyle(.white)
+            }.padding()
             List {
                 ForEach(viewModel.activeList) { event in
                     let timeInSec = event.getSecFromNow()
@@ -67,7 +104,7 @@ struct ContentView: View {
     }
 }
 
-//#Preview {
-//    let container = ModelContainer(for: Event.self)
-//    ContentView(modelContext: container.mainContext)
-//}
+#Preview {
+    let container = try! ModelContainer(for: Event.self)
+    return ContentView(modelContext: container.mainContext)
+}
